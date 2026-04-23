890a8b05442dce657a02c674efbb7c4bdbe6d321
Rajkumar <rajkumaraag5@gmail.com>
2026-04-23 14:15:43 +0530

    terraform changes

--- 2-eks/data.tf
+++ 2-eks/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "quantamvector-infra-statefile-backup"
+    bucket = "rajkumar-bucket-s3"
     key    = "quantamvector/1-network/terraform.tfstate"
     region = "ap-northeast-1"
   }
